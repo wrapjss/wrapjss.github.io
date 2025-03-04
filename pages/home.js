ad70271216c9aab6d75a2adcbcd7fe5fd2c8e6b7
wrapjss <103901449+wrapjss@users.noreply.github.com>
2025-03-04 11:34:16 -0500

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -242,5 +242,8 @@ pages.home = async function() {
   });
 	*/
 
-	(await getModule("wrap"))();
+	showPopUp("Welcome to Wraptop!",
+            "Wraptop is a client made by <a href='https://wrapjss.github.io/?user=wrap#profile'>@wrapjss</a> with usability and user comfort in mind. Wraptop offers uality of life and developmental features for users and developers alike.",
+            [["Okay", "var(--themeColor)"]]
+            );
 }
