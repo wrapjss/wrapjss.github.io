bbe2beb5a7e179bf5e3705dd61d231de7c7409ea
wrapjss <103901449+wrapjss@users.noreply.github.com>
2025-03-04 11:25:04 -0500

    Create wrap.js

---
+++ modules/wrap.js
@@ -0,0 +1,6 @@
+modules.wrap = async () => {
+  showPopUp("Welcome to Wraptop!",
+            "Wraptop is a client made by <a href='https://wrapjss.github.io/?user=wrap#profile'>@wrapjss</a> with usability and user comfort in mind. Wraptop offers uality of life and developmental features for users and developers alike.",
+            [["Okay", "var(--themeColor)"]]
+            );
+}
